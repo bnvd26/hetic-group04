89bb3250c64972701ac35bf6387df678171aebd6
Nathanael <natsuxgraph@gmail.com>
2021-07-21 10:46:21 +0200

    feat(router): add room routes & nav (#2)

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,32 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Rooms from "./pages/rooms";
+import Nav from "./components/Nav/Nav";
+import RoomDetails from "./pages/room_details";
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/rooms" component={Rooms} exact />
-      </Switch>
-    </Router>
+    <div className="flex h-screen bg-gray-200">
+      <Router>
+        <Nav />
+
+        <Switch>
+          <div className="flex-1 flex flex-col overflow-hidden mx-auto px-6 py-8">
+            <Route path="/" component={Home} exact />
+
+            <Route
+              path="/rooms"
+              render={({ match: { url } }) => (
+                <>
+                  <Route path={`${url}/`} component={Rooms} exact />
+                  <Route path={`${url}/:id`} component={RoomDetails} />
+                </>
+              )}
+            />
+          </div>
+        </Switch>
+      </Router>
+    </div>
   );
 };
 
