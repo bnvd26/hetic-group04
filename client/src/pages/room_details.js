a5817ee75fc2dbc9765b0a47c9d82d9639d746b0
NatsuDzn <natsuxgraph@gmail.com>
2021-07-22 10:36:06 +0200

    feat(components): add ProgressSpinner

--- client/src/pages/room_details.js
+++ client/src/pages/room_details.js
@@ -5,10 +5,12 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock } from "@fortawesome/free-solid-svg-icons";
+import ProgressSpinner from "../components/ProgressSpinner/ProgressSpinner";
 
 const RoomDetails = () => {
   const { id } = useParams();
   const [room, setRoom] = useState(null);
+  const [loading, setIsLoading] = useState(true);
   const [currentDate, setCurrentDate] = useState(new Date().toLocaleString());
   const history = useHistory();
 
@@ -22,6 +24,7 @@ const RoomDetails = () => {
       })
         .then((res) => {
           setRoom(res.data);
+          setIsLoading(false);
           console.log(res.data);
         })
         .catch((error) => {
@@ -31,32 +34,35 @@ const RoomDetails = () => {
     }
   });
 
-  if (!room) return null;
-
   return (
-    <div className="flex justify-between px-6 py-8">
-      <div className="bg-gray-200 w-full lg:max-w-sm flex flex-col h-calc px-6 pt-8 rounded-lg">
-        <h1 className="text-gray-700 text-3xl font-medium">
-          Salle: {room.name}
-        </h1>
-        <span className="flex flex-row items-center my-2">
-          <FontAwesomeIcon icon={faClock} className="text-lg mr-2" />
-          <p>{currentDate}</p>
-        </span>
+    <div>
+      {loading === true ? (
+        <ProgressSpinner />
+      ) : (
+        <div className="flex justify-between px-6 py-8">
+          <div className="bg-gray-200 w-full lg:max-w-sm flex flex-col h-calc px-6 pt-8 rounded-lg">
+            <h1 className="text-gray-700 text-3xl font-medium">
+              Salle: {room.name}
+            </h1>
+            <span className="flex flex-row items-center my-2">
+              <FontAwesomeIcon icon={faClock} className="text-lg mr-2" />
+              <p>{currentDate}</p>
+            </span>
 
-        <div
-          className="rounded-lg h-64 bg-cover"
-          style={{
-            backgroundImage:
-              "url(https://source.unsplash.com/256x256/?classroom)",
-          }}
-        ></div>
-      </div>
-
-      <div className="flex w-full flex-col m-8 my-0">
-        <div className="h-4/6 w-full m-2 mt-0 rounded-lg bg-gray-200"></div>
-        <div className="h-2/6 w-full m-2 mb-0 rounded-lg bg-gray-200"></div>
-      </div>
+            <div
+              className="rounded-lg h-64 bg-cover"
+              style={{
+                backgroundImage:
+                  "url(https://source.unsplash.com/256x256/?classroom)",
+              }}
+            ></div>
+          </div>
+          <div className="flex w-full flex-col m-8 my-0">
+            <div className="h-4/6 w-full m-2 mt-0 rounded-lg bg-gray-200"></div>
+            <div className="h-2/6 w-full m-2 mb-0 rounded-lg bg-gray-200"></div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
