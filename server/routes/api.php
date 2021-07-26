5e5b4d97c8d5f1fdb4f88d1ee2600e89cde04799
wasp84562 <quentin.monteferrario@lasuiteandco.com>
2021-07-26 10:36:09 +0200

    routing schedule

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\ScheduleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
